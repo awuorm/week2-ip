8d0030e27af6dc0b97a85cb6359e7edd9eefa391
awuorm <otieno.awuor.m@gmail.com>
2022-10-18 22:33:09 +0300

    refactored add element

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-document.body.onload = addElement;
+let assignedName = "";
+document.body.onblur = addElement;
+function addElement (assignedName) {
+    // create a new div element
+    const newDiv = document.createElement("div");
+    // and give it some content
+    const newContent = document.createTextNode("Your Ghanaian Name is:"+ assignedName);
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("output-name");
+    document.body.insertBefore(newDiv, currentDiv);
+}
+
 function getVal() {
     let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
@@ -9,46 +22,15 @@ function getVal() {
     let dayOfWeek = birthday.getDay();
     if (gender == "female") {
         assignedName = femaleNames[dayOfWeek];
-
-        const newDiv = document.createElement("div");
-        // and give it some content
-        const newContent = document.createTextNode("Your Ghanaian Names is:" + assignedName);
-        // add the text node to the newly created div
-        newDiv.appendChild(newContent);
-        // add the newly created element and its content into the DOM
-        const currentDiv = document.getElementById("output-name");
-        document.body.insertBefore(newDiv, currentDiv);
+        addElement(assignedName);
     }
     else {
         assignedName = maleNames[dayOfWeek];
-
-        const newDiv = document.createElement("div");
-        // and give it some content
-        const newContent = document.createTextNode("Your Ghanaian Names is:" + assignedName);
-        // add the text node to the newly created div
-        newDiv.appendChild(newContent);
-        // add the newly created element and its content into the DOM
-        const currentDiv = document.getElementById("output-name");
-        document.body.insertBefore(newDiv, currentDiv);
-
+        addElement(assignedName);
     }
 
     return assignedName;
 }
-// document.body.onload = addElement;
-// function addElement() {
-//     // create a new div element
-//     const newDiv = document.createElement("div");
 
-//     // and give it some content
-//     const newContent = document.createTextNode("Your Ghanaian Names is:`{assignedName}`");
-
-//     // add the text node to the newly created div
-//     newDiv.appendChild(newContent);
-
-//     // add the newly created element and its content into the DOM
-//     const currentDiv = document.getElementById("output-name");
-//     document.body.insertBefore(newDiv, currentDiv);
-// }
 
 
