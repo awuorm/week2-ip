4d86dd337abe0549dbe1beccc1f72ad4f35e5c4e
awuorm <otieno.awuor.m@gmail.com>
2022-10-20 21:31:35 +0300

    changed form background color, added image

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 let assignedName = "";
 document.body.onblur = addElement;
-function addElement (assignedName) {
+function addElement(assignedName) {
+    console.log(assignedName, "assigned Name")
     // create a new div element
     const newDiv = document.createElement("div");
-    // and give it some content
-    const newContent = document.createTextNode("Your Akan Name is"+ assignedName);
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-    // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("output-name");
-    document.body.insertBefore(newDiv, currentDiv);
+    // validate user input
+    if (assignedName == undefined || typeof (assignedName) != typeof ("assignedName")) {
+        const newContent = document.createTextNode("Your Akan Name is still being calculated");
+        // add the text node to the newly created div
+        newDiv.appendChild(newContent);
+        // add the newly created element and its content into the DOM
+        const currentDiv = document.getElementById("output-name");
+        document.body.insertBefore(newDiv, currentDiv); 
+    }
+    else{
+        const newContent = document.createTextNode("Your Akan Name is " + assignedName);
+        // add the text node to the newly created div
+        newDiv.appendChild(newContent);
+        // add the newly created element and its content into the DOM
+        const currentDiv = document.getElementById("output-name");
+        document.body.insertBefore(newDiv, currentDiv);
+    }
+    
 }
 
 function getVal() {
     let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    let assignedName = "";
     let dob = document.getElementById('Enter-date').value;
     let gender = document.getElementById('Enter-gender').value;
     gender = gender.toLowerCase()
